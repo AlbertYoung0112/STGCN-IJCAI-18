84de1d7a2b52d1af3b49517d139b770613986746
Albert Young <1004018919@qq.com>
2020-10-30 16:57:02 +0800

    Adapt the proj

--- main.py
+++ main.py
@@ -8,7 +8,7 @@
 
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from os.path import join as pjoin
 
 import tensorflow as tf
@@ -27,16 +27,18 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--n_route', type=int, default=228)
 parser.add_argument('--n_his', type=int, default=12)
-parser.add_argument('--n_pred', type=int, default=9)
-parser.add_argument('--batch_size', type=int, default=50)
-parser.add_argument('--epoch', type=int, default=50)
-parser.add_argument('--save', type=int, default=10)
+parser.add_argument('--n_pred', type=int, default=3)
+parser.add_argument('--batch_size', type=int, default=10)
+parser.add_argument('--epoch', type=int, default=500)
+parser.add_argument('--save', type=int, default=100)
 parser.add_argument('--ks', type=int, default=3)
 parser.add_argument('--kt', type=int, default=3)
 parser.add_argument('--lr', type=float, default=1e-3)
 parser.add_argument('--opt', type=str, default='RMSProp')
 parser.add_argument('--graph', type=str, default='default')
 parser.add_argument('--inf_mode', type=str, default='merge')
+parser.add_argument('--weight', type=str, default='')
+parser.add_argument('--data', type=str, default='')
 
 args = parser.parse_args()
 print(f'Training configs: {args}')
@@ -49,9 +51,11 @@ blocks = [[1, 32, 64], [64, 32, 128]]
 # Load wighted adjacency matrix W
 if args.graph == 'default':
     W = weight_matrix(pjoin('./dataset', f'PeMSD7_W_{n}.csv'))
+    data_file = f'PeMSD7_V_{n}.csv'
 else:
     # load customized graph weight matrix
-    W = weight_matrix(pjoin('./dataset', args.graph))
+    W = weight_matrix(args.graph, scaling=False)
+    data_file = args.data
 
 # Calculate graph kernel
 L = scaled_laplacian(W)
@@ -60,9 +64,8 @@ Lk = cheb_poly_approx(L, Ks, n)
 tf.add_to_collection(name='graph_kernel', value=tf.cast(tf.constant(Lk), tf.float32))
 
 # Data Preprocessing
-data_file = f'PeMSD7_V_{n}.csv'
-n_train, n_val, n_test = 34, 5, 5
-PeMS = data_gen(pjoin('./dataset', data_file), (n_train, n_val, n_test), n, n_his + n_pred)
+n_train, n_val, n_test = 50, 5, 5
+PeMS = data_gen(data_file, (n_train, n_val, n_test), n, n_his + n_pred, 24)
 print(f'>> Loading dataset with Mean: {PeMS.mean:.2f}, STD: {PeMS.std:.2f}')
 
 if __name__ == '__main__':
